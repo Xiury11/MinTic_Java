37ae7228db0e82e64bd6c02c07fcc2ddffb77b90
Xiury11 <xiuryn1195@gmail.com>
2021-07-26 19:07:32 -0500

    commit

--- src/views/Files.java
+++ src/views/Files.java
@@ -1,7 +1,7 @@
 package views;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Files{
@@ -9,23 +9,38 @@ public class Files{
     public static void main(String[] args) {
 
         try {
-
-            OutputStream ous = new FileOutputStream("./data/data.properties");
-
-            Properties  prop = new Properties();
-
-            prop.setProperty("nombre", "Xiury");
-            prop.setProperty("edad", "25");
-
-            prop.store(ous, null);
-
-            System.out.println(prop);
-
-
             
+            InputStream ins = new FileInputStream("./data/data.properties");
+            Properties prop = new Properties();
+            prop.load(ins);
+            System.out.println(prop.getProperty("58"));
+
         } catch (Exception e) {
             //TODO: handle exception
         }
+
+        // try {
+
+        //     OutputStream ous = new FileOutputStream("./data/data.properties");
+
+        //     Properties  prop = new Properties();
+
+        //     //prop.setProperty("nombre", "Xiury");
+        //     //prop.setProperty("edad", "25");
+
+        //     for (int i = 1; i <= 100; i++){
+        //         prop.setProperty(i+"", (i*3)+"");
+        //     }
+
+        //     prop.store(ous, null);
+
+        //     System.out.println(prop);
+
+
+            
+        // } catch (Exception e) {
+        //     //TODO: handle exception
+        // }
     
     }
 
